1ab7af1899aea8a3dcf9d56d032e0514be1d8466
Ian Ginnaty <imekigin@gmail.com>
2016-07-26 01:03:30 +0000

    finally got schema fixed

--- db/schema.rb
+++ db/schema.rb
@@ -11,50 +11,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160718191650) do
+ActiveRecord::Schema.define(version: 20160725200651) do
 
   create_table "availabilities", force: :cascade do |t|
-    t.integer  "item_id",      null: false
-    t.string   "cron_string",  null: false
-    t.datetime "created_time", null: false
-    t.datetime "updated_time"
+    t.integer "item_id",     null: false
+    t.string  "cron_string", null: false
   end
 
   add_index "availabilities", ["item_id"], name: "index_availabilities_on_item_id"
 
   create_table "checkouts", force: :cascade do |t|
-    t.integer  "item_id",           null: false
-    t.integer  "user_id",           null: false
-    t.string   "checkout_time_out", null: false
-    t.string   "checkout_time_in"
-    t.datetime "created_time",      null: false
-    t.datetime "updated_time"
+    t.integer "item_id",           null: false
+    t.integer "user_id",           null: false
+    t.string  "checkout_time_out", null: false
+    t.string  "checkout_time_in",  null: false
   end
 
   add_index "checkouts", ["item_id"], name: "index_checkouts_on_item_id"
   add_index "checkouts", ["user_id"], name: "index_checkouts_on_user_id"
 
   create_table "comments", force: :cascade do |t|
-    t.integer  "item_id",      null: false
-    t.integer  "user_id",      null: false
-    t.string   "comment",      null: false
-    t.datetime "created_time", null: false
-    t.datetime "updated_time"
+    t.integer "item_id", null: false
+    t.integer "user_id", null: false
+    t.string  "comment", null: false
   end
 
   add_index "comments", ["item_id"], name: "index_comments_on_item_id"
   add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "items", force: :cascade do |t|
-    t.integer  "user_id",      null: false
-    t.string   "title",        null: false
-    t.string   "description",  null: false
-    t.string   "price",        null: false
-    t.string   "location",     null: false
-    t.string   "condition",    null: false
-    t.string   "status",       null: false
-    t.datetime "created_time", null: false
-    t.datetime "updated_time"
+    t.integer "user_id",     null: false
+    t.string  "title",       null: false
+    t.string  "description", null: false
+    t.string  "price",       null: false
+    t.string  "location",    null: false
+    t.string  "condition",   null: false
+    t.string  "status",      null: false
   end
 
   add_index "items", ["user_id"], name: "index_items_on_user_id"
@@ -67,13 +59,11 @@ ActiveRecord::Schema.define(version: 20160718191650) do
   add_index "tags", ["item_id"], name: "index_tags_on_item_id"
 
   create_table "users", force: :cascade do |t|
-    t.string   "firstname",    null: false
-    t.string   "lastname",     null: false
-    t.string   "user_name",    null: false
-    t.string   "address",      null: false
-    t.integer  "phone",        null: false
-    t.datetime "created_time", null: false
-    t.datetime "updated_time"
+    t.string  "firstname", null: false
+    t.string  "lastname",  null: false
+    t.string  "user_name", null: false
+    t.string  "address",   null: false
+    t.integer "phone",     null: false
   end
 
 end
