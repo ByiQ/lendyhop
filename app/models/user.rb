5592d718f1f6c0ea4a81964ceab3cfd1194a5b52
Ian Ginnaty <imekigin@gmail.com>
2016-07-26 00:11:50 +0000

    att_accessible commented out

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,3 @@
 class User < ActiveRecord::Base
-    attr_accessible :firstname, :lastname, :user_name, :address, :phone, :created_time, :updated_time
+    # attr_accessible :firstname, :lastname, :user_name, :address, :phone, :created_time, :updated_time
 end
