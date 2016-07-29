6c44ecab185eb1f4832d7ff2f2d34f59d796e223
Ian Ginnaty <imekigin@gmail.com>
2016-07-29 22:51:13 +0000

    format routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,8 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
-  resources :users, :items
+  resources :users
+  resources :items
   
   # Example resource route with options:
   #   resources :products do
