90a6dfe1d4541635a7e4247fff42a54eb43f7a0e
Ian Ginnaty <imekigin@gmail.com>
2016-07-30 02:43:43 +0000

    .

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-    #def to_param
-    #    user_name
-    #end
+    def to_param
+        user_name
+    end
     # attr_accessible :firstname, :lastname, :user_name, :address, :phone, :created_time, :updated_time
 end
