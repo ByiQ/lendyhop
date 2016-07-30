09be82e7742e647de7f6f23d94b0c322079de786
Ian Ginnaty <imekigin@gmail.com>
2016-07-30 02:09:08 +0000

    login almost working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
   end
   
   def verify
-    @user = User.where(username: params[:user_name])
+    @user = User.where(user_name: params[:user_name])
     # session[:userID] = @user.id
     flash[:notice] = "Successful login: #{@user.user_name}."
     redirect_to user_path(@user)
