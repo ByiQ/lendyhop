808ee687e2b9f5e0eaa2cfa018df1758665d35ec
Ian Ginnaty <imekigin@gmail.com>
2016-08-09 21:15:58 +0000

    tags complete; updating schema to reflect new checkout method

--- db/migrate/20160725200651_create_users.rb
+++ db/migrate/20160725200651_create_users.rb
@@ -1,39 +1,39 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :firstname,      null: false
-      t.string :lastname,       null: false
-      t.string :user_name,      null: false
-      t.string :address,        null: false
-      t.integer :phone,         null: false
+      t.string :firstname, null: false
+      t.string :lastname,  null: false
+      t.string :user_name, null: false
+      t.string :address,   null: false
+      t.integer :phone,    null: false
     end
     
     create_table :items do |t|
-      t.belongs_to :user,          null: false, index: true, foreign_key: true
-      t.string :title,             null: false
-      t.string :description,       null: false
-      t.string :price,             null: false
-      t.string :location,          null: false
-      t.string :condition,         null: false
-      t.string :status,            null: false
+      t.belongs_to :user,    null: false, index: true, foreign_key: true
+      t.string :title,       null: false
+      t.string :description, null: false
+      t.string :price,       null: false
+      t.string :location,    null: false
+      t.string :condition,   null: false
+      t.string :status,      null: false
     end
     
     create_table :checkouts do |t|
-      t.belongs_to :item,          null: false, index: true, foreign_key: true
-      t.belongs_to :user,          null: false, index: true, foreign_key: true
-      t.string :checkout_time_out, null: false
-      t.string :checkout_time_in,  null: false
+      t.belongs_to :item,  null: false, index: true, foreign_key: true
+      t.belongs_to :user,  null: false, index: true, foreign_key: true
+      t.integer :bintime,  null: false
     end
     
     create_table :comments do |t|
-      t.belongs_to :item,          null: false, index: true, foreign_key: true
-      t.belongs_to :user,          null: false, index: true, foreign_key: true
-      t.string :comment,           null: false
+      t.belongs_to :item, null: false, index: true, foreign_key: true
+      t.belongs_to :user, null: false, index: true, foreign_key: true
+      t.string :comment,  null: false
     end
     
     create_table :availabilities do |t|
-      t.belongs_to :item,          null: false, index: true, foreign_key: true
-      t.string :cron_string,       null: false
+      t.belongs_to :item,    null: false, index: true, foreign_key: true
+      t.string :cron_string, null: false
+      t.integer :bintime,    null: false
     end
     
     create_table :tags do |t|
