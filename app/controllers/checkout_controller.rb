99d4e2f457aeac5507514388af9818a03a8dbb37
Ian Ginnaty <imekigin@gmail.com>
2016-08-09 23:57:44 +0000

    bitwise ops are bad in ruby

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -25,7 +25,7 @@ class CheckoutsController < ApplicationController
     @taken = {}
     @days.reverse.each do |prefix|
       @slots.reverse.each do |suffix|
-        @taken[prefix + suffix] = totalbintime.downto(0)
+        @taken[prefix + suffix] = (totalbintime >> 1)[0]
         totalbintime >> 1
       end
     end
