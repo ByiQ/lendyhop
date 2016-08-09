f17ef601a31607e07b11b3d3617150db29ba8f0a
Ian Ginnaty <imekigin@gmail.com>
2016-08-09 22:12:31 +0000

    cleaner bintime

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -6,28 +6,14 @@ class CheckoutsController < ApplicationController
 
   def create
     pars = params
+    days = [ 'm', 't', 'w', 'r', 'f', 's', 'u' ]
+    slots = [ 'm', 'a', 'e' ]
     bintime = 0b0
-    bintime = (bintime | (pars['mm'].to_i & 1)) << 1
-    bintime = (bintime | (pars['ma'].to_i & 1)) << 1
-    bintime = (bintime | (pars['me'].to_i & 1)) << 1
-    bintime = (bintime | (pars['tm'].to_i & 1)) << 1
-    bintime = (bintime | (pars['ta'].to_i & 1)) << 1
-    bintime = (bintime | (pars['te'].to_i & 1)) << 1
-    bintime = (bintime | (pars['wm'].to_i & 1)) << 1
-    bintime = (bintime | (pars['wa'].to_i & 1)) << 1
-    bintime = (bintime | (pars['we'].to_i & 1)) << 1
-    bintime = (bintime | (pars['rm'].to_i & 1)) << 1
-    bintime = (bintime | (pars['ra'].to_i & 1)) << 1
-    bintime = (bintime | (pars['re'].to_i & 1)) << 1
-    bintime = (bintime | (pars['fm'].to_i & 1)) << 1
-    bintime = (bintime | (pars['fa'].to_i & 1)) << 1
-    bintime = (bintime | (pars['fe'].to_i & 1)) << 1
-    bintime = (bintime | (pars['sm'].to_i & 1)) << 1
-    bintime = (bintime | (pars['sa'].to_i & 1)) << 1
-    bintime = (bintime | (pars['se'].to_i & 1)) << 1
-    bintime = (bintime | (pars['um'].to_i & 1)) << 1
-    bintime = (bintime | (pars['ua'].to_i & 1)) << 1
-    bintime = (bintime | (pars['ue'].to_i & 1)) << 1
+    days.each do |prefix|
+      slots.each do |suffix|
+        bintime = (bintime | (pars[prefix << suffix].to_i & 1)) << 1
+      end
+    end
     puts bintime
   end
 
