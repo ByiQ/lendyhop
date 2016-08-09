51b83b87e4645a04fee06a6861191a98dd0731ca
Ian Ginnaty <imekigin@gmail.com>
2016-08-09 22:09:19 +0000

    stoi

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -7,27 +7,27 @@ class CheckoutsController < ApplicationController
   def create
     pars = params
     bintime = 0b0
-    bintime = (bintime | pars['mm']) << 1
-    bintime = (bintime | pars['ma']) << 1
-    bintime = (bintime | pars['me']) << 1
-    bintime = (bintime | pars['tm']) << 1
-    bintime = (bintime | pars['ta']) << 1
-    bintime = (bintime | pars['te']) << 1
-    bintime = (bintime | pars['wm']) << 1
-    bintime = (bintime | pars['wa']) << 1
-    bintime = (bintime | pars['we']) << 1
-    bintime = (bintime | pars['rm']) << 1
-    bintime = (bintime | pars['ra']) << 1
-    bintime = (bintime | pars['re']) << 1
-    bintime = (bintime | pars['fm']) << 1
-    bintime = (bintime | pars['fa']) << 1
-    bintime = (bintime | pars['fe']) << 1
-    bintime = (bintime | pars['sm']) << 1
-    bintime = (bintime | pars['sa']) << 1
-    bintime = (bintime | pars['se']) << 1
-    bintime = (bintime | pars['um']) << 1
-    bintime = (bintime | pars['ua']) << 1
-    bintime = (bintime | pars['ue']) << 1
+    bintime = (bintime | (pars['mm'].to_i & 1)) << 1
+    bintime = (bintime | (pars['ma'].to_i & 1)) << 1
+    bintime = (bintime | (pars['me'].to_i & 1)) << 1
+    bintime = (bintime | (pars['tm'].to_i & 1)) << 1
+    bintime = (bintime | (pars['ta'].to_i & 1)) << 1
+    bintime = (bintime | (pars['te'].to_i & 1)) << 1
+    bintime = (bintime | (pars['wm'].to_i & 1)) << 1
+    bintime = (bintime | (pars['wa'].to_i & 1)) << 1
+    bintime = (bintime | (pars['we'].to_i & 1)) << 1
+    bintime = (bintime | (pars['rm'].to_i & 1)) << 1
+    bintime = (bintime | (pars['ra'].to_i & 1)) << 1
+    bintime = (bintime | (pars['re'].to_i & 1)) << 1
+    bintime = (bintime | (pars['fm'].to_i & 1)) << 1
+    bintime = (bintime | (pars['fa'].to_i & 1)) << 1
+    bintime = (bintime | (pars['fe'].to_i & 1)) << 1
+    bintime = (bintime | (pars['sm'].to_i & 1)) << 1
+    bintime = (bintime | (pars['sa'].to_i & 1)) << 1
+    bintime = (bintime | (pars['se'].to_i & 1)) << 1
+    bintime = (bintime | (pars['um'].to_i & 1)) << 1
+    bintime = (bintime | (pars['ua'].to_i & 1)) << 1
+    bintime = (bintime | (pars['ue'].to_i & 1)) << 1
     puts bintime
   end
 
