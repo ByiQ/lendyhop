f4996f74a93d3392ca2cd6ce376c89d3890c2155
Ian Ginnaty <imekigin@gmail.com>
2016-08-09 21:00:40 +0000

    delete should redirect

--- app/controllers/tag_controller.rb
+++ app/controllers/tag_controller.rb
@@ -12,5 +12,11 @@ class TagsController < ApplicationController
     @tag = Tag.create!(@par)
     redirect_to item_path(@par[:item_id])
   end
+  
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+    redirect_to item_path(params[:item_id])
+  end
 
 end
