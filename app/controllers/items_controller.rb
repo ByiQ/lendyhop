327dbcbd789403b5096c2713c8cfe84271900083
Ian Ginnaty <imekigin@gmail.com>
2016-08-11 00:48:25 +0000

    .

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -90,7 +90,7 @@ class ItemsController < ApplicationController
       @items[-1].attributes[:scount] = count
     end
     
-    @pars = params[:terms]["terms"]
+    @pars = params[:terms]
     @terms = Object.new
 
     @terms.define_singleton_method(:terms) do
