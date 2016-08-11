94cafee363179d605de62aa57ff36d6ef6cab803
Ian Ginnaty <imekigin@gmail.com>
2016-08-11 00:09:56 +0000

    .

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -89,7 +89,8 @@ class ItemsController < ApplicationController
       @items.push(Item.find(id))
       @items[-1].attributes[:scount] = count
     end
-    @terms = params
+    
+    @terms = params[:terms]
   end
 
   private
