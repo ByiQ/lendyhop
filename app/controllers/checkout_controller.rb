3f0c6074e770b710c770902d938a574046e1eccb
Ian Ginnaty <imekigin@gmail.com>
2016-08-12 03:22:51 +0000

    var scoped wrong

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -42,8 +42,8 @@ class CheckoutsController < ApplicationController
         bintime = (bintime | (pars[prefix + suffix].to_i & 1)) << 1
       end
     end
+    @par = checkout_params
     if (bintime > 0)
-      @par = checkout_params
       @par[:bintime] = bintime
       @checkout = Checkout.create!(@par)
       flash[:notice] = "Item successfully checked out"
